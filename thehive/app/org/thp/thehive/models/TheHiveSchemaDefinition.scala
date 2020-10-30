2503cc565976d60fdd0d9e1db44365ff2c9830d9
Robin Riclet <robinriclet@gmail.com>
2020-10-30 14:45:24 +0100

    Deleted edge removal when cascade deleting a Share

--- thehive/app/org/thp/thehive/models/TheHiveSchemaDefinition.scala
+++ thehive/app/org/thp/thehive/models/TheHiveSchemaDefinition.scala
@@ -72,7 +72,8 @@ class TheHiveSchemaDefinition @Inject() extends Schema with UpdatableSchema {
       traversal.unsafeHas("status", "Deleted").remove()
       Success(())
     }
-    .updateGraph("Remove deleted property from logs", "Log") { traversal =>
+    .updateGraph("Remove deleted logs and deleted property from logs", "Log") { traversal =>
+      traversal.unsafeHas("deleted", "true").remove()
       traversal.removeProperty("deleted")
       Success(())
     }
